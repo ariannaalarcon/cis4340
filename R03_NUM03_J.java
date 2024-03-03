27ffafe0fe29ab09b39c10c85575a1a180633456
ariannaalarcon <87721121+ariannaalarcon@users.noreply.github.com>
2024-03-03 14:24:29 -0500

    Create R03_NUM03_J.java

---
+++ R03_NUM03_J.java
@@ -0,0 +1,16 @@
+/* Rule 03: Numeric Types and Operations (NUM)
+NUM03-J. Use integer types that can fully represent the possible range of unsigned data
+
+Non-compliant code */
+
+public static int getInteger(DataInputStream is) throws IOException {
+    return is.readInt();
+}
+
+/* Corrected Code */
+
+public static long getInteger(DataInputStream is) throws IOException {
+    return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
+}
+
+
